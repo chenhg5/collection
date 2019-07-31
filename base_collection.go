75ca92468bae81b844f502cbfbf241da16e6e1c1
cg33 <chg80333@163.com>
2019-07-31 17:37:21 +0800

    rename package

--- base_collection.go
+++ base_collection.go
@@ -1,4 +1,4 @@
-package main
+package collection
 
 import "github.com/shopspring/decimal"
 
--- muti_dimensional_array_collection.go
+++ muti_dimensional_array_collection.go
@@ -1,4 +1,4 @@
-package main
+package collection
 
 type MultiDimensionalArrayCollection struct {
 	value [][]interface{}
